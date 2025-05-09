77ea01e9555fee88548c2e257131a12ba0c96132
czyczy <2937174473@qq.com>
2025-05-09 13:04:43 +0800

    1

---
+++ Config.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+# 图片及视频检测结果保存路径
+save_path = 'save_data'
+
+# 使用的模型路径
+model_path = 'models/best.pt'
+names = {0: 'cargo', 1: 'passenger', 2: 'cruise', 3: 'bulker', 4: 'tanker', 5: 'sailingboat', 6: 'tug', 7: 'fishing', 8: 'drill', 9: 'firefighting',10: 'containership',11:'warship',12:'submarine'}
+
+CH_names = ['货船', '客船', '游轮', '散货船', '油轮', '帆船','拖船','渔船','钻井船','消防船','集装箱船','军舰','潜艇']
